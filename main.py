6c29e5e3aebb24d1fca555a8953a9707786e0777
feruz4200 <f.bek570@gmail.com>
2025-08-07 19:06:31 +0500

    main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ async def handle_join_request(update: Update, context: ContextTypes.DEFAULT_TYPE
     join_request = update.chat_join_request
     user_id = join_request.from_user.id
 
- await context.bot.send_message(
-    chat_id=user_id,
-    text="👋 Assalomu alaykum! QARINDOSH \nSiz kanalga qo‘shilish uchun so‘rov yubordingiz.\nZayavkangiz qabul qilinyapti!"
-)
+    await context.bot.send_message(
+        chat_id=user_id,
+        text="👋 Assalomu alaykum! QARINDOSH \nSiz kanalga qo‘shilish uchun so‘rov yubordingiz.\nZayavkangiz qabul qilinyapti!"
+    )
+
     await context.bot.approve_chat_join_request(
         chat_id=join_request.chat.id,
         user_id=user_id
@@ -20,7 +21,7 @@ async def handle_join_request(update: Update, context: ContextTypes.DEFAULT_TYPE
 async def main():
     application = ApplicationBuilder().token("8477466598:AAGfAxcxlZ2o0SKOQROMw3EyqmsVjXWnHLM").build()
     application.add_handler(ChatJoinRequestHandler(handle_join_request))
-    print("🤖 Bot ishga tushdi")
+    print("✅ Bot ishga tushdi")
     await application.run_polling()
 
 if __name__ == "__main__":
